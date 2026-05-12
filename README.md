5dd9e6463538761029d31c53c1f27b88aaba37e4
Frank Delporte <fdelporte@azul.com>
2026-05-12 16:29:49 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Demo using Azul Zulu with Paketo Buildpacks
+
+**Minimal Maven application to demonstrate the use of Azul Zulu with Paketo Buildpacks.**
+
+## More Information
+
+This repository contains the demo files explained in the blog post [TODO](https://www.azul.com/blog/using-the-azul-zulu-docker-official-images-from-simple-pull-to-lean-container/).
+
+
---
+++ src/main/java/be/webtechie/PaketoApplication.java
@@ -0,0 +1,39 @@
+package be.webtechie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Minimal Spring Boot application to demonstrate Paketo Buildpacks with Azul Zulu.
+ *
+ * Build the container image (no Dockerfile required):
+ *   ./mvnw spring-boot:build-image
+ *
+ * Run the container:
+ *   docker run --rm -p 8080:8080 paketo-demo
+ *
+ * Check the Java version inside:
+ *   docker run --rm paketo-demo java -version
+ */
+@SpringBootApplication
+@RestController
+public class PaketoApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(PaketoApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello from Azul Zulu via Paketo!";
+    }
+
+    @GetMapping("/version")
+    public String version() {
+        return "Java " + System.getProperty("java.version")
+                + " (" + System.getProperty("java.vendor") + " "
+                + System.getProperty("java.vendor.version") + ")";
+    }
+}
